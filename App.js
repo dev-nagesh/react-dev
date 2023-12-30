ef5836b0cdf9b39175e2d3ac5743981a55429fa1
Nagesh Babu Vanukuru <dev.nagesh.vnk@gmail.com>
2023-12-30 12:04:07 +0530

    Namaste React Episode 3

--- App.js
+++ App.js
@@ -1,26 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",{id:"child1"},
-            [
-                React.createElement("h1",{id:"heading11"},"Child1 - heading 1"),
-                React.createElement("h2",{id:"child12"},"Child1 - heading 2")
-            ]
-        ),
-        React.createElement(
-            "div",{id:"child2"},
-            [
-                React.createElement("h1",{id:"heading21"},"Child2 - heading 1"),
-                React.createElement("h2",{id:"heading22"},"Child2 - heading 2")
-            ]
-        )
-    ]
+
+// JSX (Transpiled before it reaches the JS) - Parcel -Babel
+
+// JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTMLElement(renderer)
+// const jsxheading = (
+//     <h1 className="head" tabIndex="1">
+//         Hello Namaste React Heading
+//     </h1>
+// )
+// console.log(jsxheading);
+
+const Heading = () => {
+    return <h1 className="heading" tabIndex="1">Namaste React Heading</h1>
+}
+
+// ReACT Component Name must always have Capital Letter
+// React Functional component is a javascript arrow function which returns a piece of JSX code which is React Element
+/*const Title = () => <h1 className="heading" tabIndex="0">  Namaste React Heading</h1>
+
+// Component Composition : Includingor using one React component inside another component
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading" tabIndex="1"> React Functional Component</h1>
+    </div>
 )
-//const heading = React.createElement("h1",{id:"heading"},"Hello, Namaste React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />); */
+
+/* const title =  (<h1 className="heading" tabIndex="0">  Namaste React Heading</h1>)
+
+// Component Composition : Includingor using one React component inside another component
+const HeadingComponent = () => (
+    <div id="container">
+        {title}
+        <h1 className="heading" tabIndex="1"> React Functional Component</h1>
+    </div>
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />); */
+// Component Composition : Includingor using one React component inside another component
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading" tabIndex="1"> React Functional Component</h1>
+    </div>
+)
+
+const title =  (
+    <h1 className="heading" tabIndex="0">
+        <HeadingComponent />
+        <HeadingComponent></HeadingComponent>
+        {HeadingComponent()}
+        {100 + 200}
+         Namaste React Heading
+    </h1>
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(title);
